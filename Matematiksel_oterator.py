e7a417bc947fe5f94cb23805f1502d81992fcd0a
Ömer Faruk Ballı <71135790+omerfarballi@users.noreply.github.com>
2021-03-07 17:16:12 +0300

    Add files via upload

---
+++ Matematiksel_oterator.py
@@ -0,0 +1,10 @@
+print("toplama/çıkarma/çarpma")
+print(96+85*(100/32))
+print("bölme")
+print(25/23)#1.0025
+print("üstel ifade")
+print(2**8)#2^8 = 256
+print("mod")
+print(25%5)#  0
+print("Ondalıksız bölme")
+print(150//23)#6
---
+++ intiger&float.py
@@ -0,0 +1,3 @@
+print(type(52))
+print(type(50.85))
+print(type(50.859878545651564))
